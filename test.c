db274dc0a9dc79350c87062fd778cdd1a4c51f42
Trafalgar99 <1286015267@qq.com>
2020-05-06 19:15:26 +0800

    m

---
+++ test.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#define MSG "I　am　a　symbolic　string　constant"
+#define MAXLENGTH 81
+
+int main()
+{
+    char words[MAXLENGTH] = "I　am　a　string　in　an　array";
+    const char *pt1 = "Something is pointing at me.";
+    puts("Here　are　some　strings:");
+    puts(MSG);
+    puts(words);
+    puts(pt1);
+    words[8] = 'p';
+    puts(words);
+    printf("%s, %p, %c\n", "We", "are", *"space farers");
+
+    return 0;
+}
