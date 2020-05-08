0a565b495a7ffd279002bebf56164a5a403d50ef
Trafalgar99 <1286015267@qq.com>
2020-05-08 14:08:18 +0800

    5.08

--- test.c
+++ test.c
@@ -1,16 +1,7 @@
 #include <stdio.h>
-#define MSG "I　am　a　symbolic　string　constant"
-#define MAXLENGTH 81
 
-int main()
+int main(int argc, char *argv[])
 {
-    char *p1 = "Klingon";
-    
-    printf("Kligon");
-    printf(": Beware the %ss!\n", "Klingon");
-
-    gets_s(p1,5);
-    fgets(p1, 5, stdin);
 
     return 0;
 }
