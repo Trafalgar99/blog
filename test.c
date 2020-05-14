c9344b59daca0526d4d663780097caadc0a56642
Trafalgar99 <1286015267@qq.com>
2020-05-14 19:34:13 +0800

    5.14

--- test.c
+++ test.c
@@ -1,41 +1,19 @@
-/* binbit.c -- 使用位操作显示二进制 */
-#include <limits.h> // 提供 CHAR_BIT 的定义，CHAR_BIT 表示每字节 的位数
 #include <stdio.h>
-char *itobs(int, char *);
-void show_bstr(const char *);
-int main(void)
+void why_me();
+int main()
 {
-    char bin_str[CHAR_BIT * sizeof(int) + 1];
-    int number;
-    puts("Enter integers and see them in binary.");
-    puts("Non-numeric input terminates program.");
-    while (scanf("%d", &number) == 1)
-    {
-        itobs(number, bin_str);
-        printf("%d is ", number);
-        show_bstr(bin_str);
-        putchar('\n');
-    }
-    puts("Bye!");
+    printf("The file is %s.\n", __FILE__);
+    printf("The date is %s.\n", __DATE__);
+    printf("The time is %s.\n", __TIME__);
+    printf("The version is %ld.\n", __STDC_VERSION__);
+    printf("This is line %d.\n", __LINE__);
+    printf("This function is %s\n", __func__);
+    why_me();
     return 0;
 }
-char *itobs(int n, char *ps)
+
+void why_me()
 {
-    int i;
-    const static int size = CHAR_BIT * sizeof(int);
-    for (i = size - 1; i >= 0; i--, n >>= 1)
-        ps[i] = (01 & n) + '0';
-    ps[size] = '\0';
-    return ps;
-}
-/*4位一组显示二进制字符串 */
-void show_bstr(const char *str)
-{
-    int i = 0;
-    while (str[i]) /* 不是一个空字符 */
-    {
-        putchar(str[i]);
-        if (++i % 4 == 0 && str[i])
-            putchar(' ');
-    }
+    printf("This function is %s\n", __func__);
+    printf("This is line %d.\n", __LINE__);
 }
