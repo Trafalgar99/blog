fb47d1d2540264864e9036dd576e0fc7324ab0fc
Trafalgar99 <1286015267@qq.com>
2020-05-19 12:40:55 +0800

    5.19

--- test.c
+++ test.c
@@ -1,14 +1,28 @@
-#include <errno.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include<stdio.h>
+#define c 5
 
-char * const *(*next)();
+void prInfo(int arr[])
+{
+    printf("%d\n", sizeof arr);
+}
 
 int main(void)
 {
-    int arr[3];
-    extern int a[];
+    int arr[]={1,2,3,4,5};
+    char ss[][2]={"asd","asda"};
+    printf("%d\n", sizeof arr);
+    prInfo(arr);
+    char *p = "abcd";
+    char s[]="abcd";
+    printf("%d %d %d\n", sizeof "abcd", sizeof p, sizeof s);
+
+    int as[12]={0};
+    int (*pas)[12]=&as;
     
-    char *p="sdfsd";
+    printf("%d ", pas[0][0]);
+    typedef double D;
+    
+    const D* dd=NULL;
+
+    return 0;
 }
---
+++ test2.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+
+struct Foo{};
+
+int main(void)
+{
+
+    return 0;
+}
