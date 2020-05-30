e1ae45b03a91b4131ce2e7cc410e1ca6c8737d21
Trafalgar99 <1286015267@qq.com>
2020-05-30 13:01:06 +0800

    5.30

--- c++Primer/sales_data.cpp
+++ c++Primer/sales_data.cpp
@@ -1,8 +1,14 @@
+#include <iostream>
 #include <string>
-#include<iostream>
 
-struct Sales_data
+class Sales_data
 {
+    // 为Sales_data的非成员函数所做的友元声明 
+    friend Sales_data add (const Sales_data&, const Sales_data&);
+    friend std::istream &read(std::istream&,Sales_data&);
+    friend std::ostream &print(std::ostream&,const Sales_data&);
+
+public:
     Sales_data() = default;
     Sales_data(const std::string &s) : bookNo(s) {}
     Sales_data(const std::string &s, unsigned n, double p) : bookNo(s), units_sold(n), revenue(p * n) {}
@@ -10,9 +16,11 @@ struct Sales_data
 
     std::string isbn() const { return bookNo; }
     Sales_data &combine(const Sales_data &);
+
+private:
     double avg_price() const;
     std::string bookNo;
-    unsigned units_sold = 0;
+    unsigned units_sold{0};
     double revenue = 0.0;
 };
 
--- test2.cpp
+++ test2.cpp
@@ -2,25 +2,61 @@
 #include <string>
 #include <vector>
 
-using namespace std;
-
-typedef struct
-{
-    int a;
-} Node;
-auto func(int i) -> int (*)[10];
-
-int main(void)
-{
-
-    return 0;
-}
-
-
-class a
+class Screen
 {
 public:
-    a(int n) : num(n) {}
+    typedef std::string::size_type pos;
+    Screen() = default;
 
-    int num;
+    Screen(pos ht, pos wd, char c) : height(ht), width(wd), contents(ht * wd, c) {}
+    char get() const { return contents[cursor]; }
+    inline char get(pos ht, pos wd) const;
+    Screen &move(pos r, pos c);
+    void some_member() const;
+    Screen &set(char);
+    Screen &set(pos, pos, char);
+
+    Screen &display(std::ostream &os)
+    {
+        do_display(os);
+        return *this;
+    }
+    const Screen &display(std::ostream &os) const
+    {
+        do_display(os);
+        return *this;
+    }
+
+private:
+    pos cursor = 0;
+    pos height = 0, width = 0;
+    std::string contents;
+    mutable size_t access_ctr;
+    void do_display(std::ostream &os) const
+    {
+        os << contents;
+    }
 };
+
+void Screen::some_member() const
+{
+    ++access_ctr; // 保存一个计数值，用于记录成员函数被调用的次数
+}
+
+class Window_mgr
+{
+private:
+    std::vector<Screen> screens{Screen(24, 80, ' ')};
+};
+
+inline Screen &Screen::set(char c)
+{
+    contents[cursor] = c;
+    return *this;
+}
+
+inline Screen &Screen::set(pos r, pos col, char ch)
+{
+    contents[r * width + col] = ch;
+    return *this;
+}
