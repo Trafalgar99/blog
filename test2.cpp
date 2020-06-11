8ba2edda3f844661885f65521d2e9d285f7338cf
Trafalgar99 <1286015267@qq.com>
2020-06-11 18:30:51 +0800

    6.11

--- test2.cpp
+++ test2.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
-
-
+#include <string>
+#include <vector>
+#include <cctype>
+using namespace std;
 void test1()
-{   // 引用无法类型转换
+{ // 引用无法类型转换
     // double dval = 3.14;
     // int &rd = dval;
     // std::cout<<rd<<std::endl;
@@ -10,15 +12,18 @@ void test1()
     // 但是常量引用可以
     double dval2 = 3.14;
     const int &rd2 = dval2;
-    std::cout<<rd2<<std::endl;
+    std::cout << rd2 << std::endl;
 }
 int tr = 89;
+
 int main(int argc, char **argv)
 {
-    static int a=0;
-    constexpr int *pa = &a;
-    
-    
-
+    string s1{"sdfs"};
+    string &s2 = s1;
+    getline(cin, s1);
+    for (decltype(s1.size()) index = 0; index != s1.size() && !isspace(s1[index]); ++index)
+    {
+        s1[index] = toupper(s1[index]);
+    }
     return 0;
 }
